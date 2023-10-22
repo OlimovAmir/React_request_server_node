eae6a5d606a2b6ceaed590d70982c411904fc3f4
olimov Amir <olimov.amir@mail.ru>
2023-10-22 14:20:32 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    // Создаем асинхронную функцию для выполнения запроса
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:8000/'); // Замените URL на свой
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    // Вызываем функцию fetchData() внутри useEffect
+    fetchData();
+  }, []); // Пустой массив зависимостей означает, что эффект выполнится один раз после монтирования компонента
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data ? (
+        // Отображение данных, полученных в результате запроса
+        <ul>
+          {data.map((item, index) => (
+            <li key={index}>
+              {item.isDirectory ? `Папка: ${item.name}` : `Файл: ${item.name}`}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        // Отображение загрузки или ошибки
+        <p>Loading data...</p>
+      )}
     </div>
   );
 }
