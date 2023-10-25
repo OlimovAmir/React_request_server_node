c700ef17a1c92281777f2a327c4741cfc4685a47
olimov Amir <olimov.amir@mail.ru>
2023-10-25 08:26:35 +0300

    v6

--- src/App.js
+++ src/App.js
@@ -4,58 +4,61 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
-  const [data, setData] = useState(null);
-  const [parent, setParent] = useState('');
+  const [data, setData] = useState([]);
   const [currentPath, setCurrentPath] = useState('/');
 
-  useEffect(() => {
-    async function fetchData(path) {
-      try {
-        const response = await fetch('http://localhost:8000/?path=' + path);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+  // Определите функцию fetchData вне useEffect
+  const fetchData = async (path) => {
+    try {
+      const response = await fetch('http://localhost:8000/?path=' + path);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
+  };
 
+  useEffect(() => {
+    // Используйте fetchData для получения данных при монтировании компонента
     fetchData(currentPath);
   }, [currentPath]);
 
   const clickHandler = (event, item) => {
     event.preventDefault();
-
     if (item.isDirectory) {
       const newPath = currentPath + item.name + '/';
-      setCurrentPath(newPath); // Обновляем путь
+      setCurrentPath(() => newPath); // Обновляем currentPath с использованием функции
     }
   };
 
-  const goBack = () => {
-    // Извлекаем предыдущую директорию из текущего пути
-    const parts = currentPath.split('/');
-    
+  const goBack = (event) => {
+    event.preventDefault();
+    let parts = currentPath.split('/');
+    // Убираем пустую строку, которая появляется из-за / в конце пути
+    parts = parts.filter((part) => part !== '');
+
+    // Убираем последний элемент (папку, в которую мы перешли)
     parts.pop();
-    console.log(parts);
-    const newPath = parts.join('/') + '/';
-    console.log(newPath);
+
+    // Собираем путь обратно с / между элементами
+    const newPath = '/' + parts.join('/');
+
     setCurrentPath(newPath);
   };
 
   return (
     <div>
       <div>
-        <button onClick={goBack}>
-          <ArrowLeft size={24} /> Назад
-        </button>
+        <a href={currentPath} onClick={goBack} className='m-4'>
+          Назад
+        </a>
         <span className='ml-3'>Текущий путь: {currentPath}</span>
       </div>
-      {data ? (
+      {Array.isArray(data) && data.length > 0 ? (
         <ul>
           {data.map((item, index) => (
             <li key={index}>
